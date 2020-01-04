25e1148012eeea01177bf86792c67eee66c4257a
F. Lorton <flanders.lorton@finnarthur.com>
2020-01-04 03:03:00 -0700

    mas

--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -12,10 +12,19 @@ public class TileManager : MonoBehaviour
     private Dictionary<string, Dictionary<string, Sprite[]>> groundTypes = new Dictionary<string, Dictionary<string, Sprite[]>>();
     private bool mapDidChange = false;
 
+    GameObject CreateTile(Grid grid, int x, int y, string type, string position){
+        GameObject tile = new GameObject();
+        tile.transform.position = new Vector3(x + (float)0.5, y + (float)0.5, 1);
+        tile.transform.localScale = new Vector3((float)6.25, (float)6.25, 0);
+        tile.transform.parent = grid.transform;
+        SpriteRenderer renderer = tile.AddComponent<SpriteRenderer>();
+        Sprite sprite = groundTypes[type][position][0];
+        renderer.sprite = sprite;
+        return tile;
+    }
+
     // Start is called before the first frame update
     void Start(){
-        Grid grid = GetComponent<Grid>();
-
         // load sprites into dictionary by type and position
         Sprite[] groundSprites = Resources.LoadAll<Sprite>("Sprites/Ground");
         string[] groundTypeNames = new string[] {"grass", "sand", "dirt", "water"};
@@ -38,18 +47,12 @@ public class TileManager : MonoBehaviour
             }
         }
 
+
         // intialize initial sprite postions
+        Grid grid = GetComponent<Grid>();
         for (int x = 0; x < mapSize; x++){
             for (int y = 0; y < mapSize; y++){
-
-                GameObject tile = new GameObject();
-                tile.transform.position = new Vector3(x + (float)0.5, y + (float)0.5, 1);
-                tile.transform.localScale = new Vector3((float)6.25, (float)6.25, 0);
-                tile.transform.parent = grid.transform;
-                SpriteRenderer renderer = tile.AddComponent<SpriteRenderer>();
-                Sprite sprite = groundTypes["grass"]["full"][0];
-                renderer.sprite = sprite;
-                mapTileSprites[x, y] = tile;
+                mapTileSprites[x, y] = CreateTile(grid, x, y, "grass", "full");
                 mapTileTypes[x, y] = "grass";
             }
         }
@@ -59,32 +62,28 @@ public class TileManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // calculate edges
         if (mapDidChange) {
-
             Grid grid = GetComponent<Grid>();
-
             for (int x = 0; x < mapSize; x++){
                 for (int y = 0; y < mapSize; y++){
                     // check 4 edges for same type
                     if(x <= 0 || mapTileTypes[x - 1 , y] != mapTileTypes[x, y]){
-                        GameObject tile = new GameObject();
-                        tile.transform.position = new Vector3(x - 1 + (float)0.5, y + (float)0.5, 1);
-                        tile.transform.localScale = new Vector3((float)6.25, (float)6.25, 0);
-                        tile.transform.parent = grid.transform;
-                        SpriteRenderer renderer = tile.AddComponent<SpriteRenderer>();
-                        Sprite sprite = groundTypes["grass"]["edge"][0];
+                        GameObject tile = CreateTile(grid, x - 1, y, "grass", "edge");
                         tile.transform.Rotate(new Vector3(0, 0, 90));
-                        renderer.sprite = sprite;
                     }
-                    //if(x >= mapSize - 1 || mapTileTypes[x + 1 , y] != mapTileTypes[x, y]){
-
-                    //}
-                    //if(y <= 0 || mapTileTypes[x , y - 1] != mapTileTypes[x, y]){
-
-                    //}
-                    //if(y >= mapSize - 1|| mapTileTypes[x, y + 1] != mapTileTypes[x, y]){
-
-                    //}
+                    if(x >= mapSize - 1 || mapTileTypes[x + 1 , y] != mapTileTypes[x, y]){
+                        GameObject tile = CreateTile(grid, x + 1, y, "grass", "edge");
+                        tile.transform.Rotate(new Vector3(0, 0, -90));
+                    }
+                    if(y <= 0 || mapTileTypes[x , y - 1] != mapTileTypes[x, y]){
+                        GameObject tile = CreateTile(grid, x, y - 1, "grass", "edge");
+                        tile.transform.Rotate(new Vector3(0, 0, 180));
+                    }
+                    if(y >= mapSize - 1|| mapTileTypes[x, y + 1] != mapTileTypes[x, y]){
+                        GameObject tile = CreateTile(grid, x, y + 1, "grass", "edge");
+                        // tile.transform.Rotate(new Vector3(0, 0, 0));
+                    }
                 }
             }
         }
