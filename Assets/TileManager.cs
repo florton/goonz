5b74816e2d8186b95ba8a445b51b52e75f52d662
F. Lorton <flanders.lorton@finnarthur.com>
2020-01-04 20:01:47 -0700

    edged map todo create tile clas to manage all

--- Assets/TileManager.cs
+++ Assets/TileManager.cs
@@ -8,7 +8,7 @@ public class TileManager : MonoBehaviour
     static public int mapSize = 10;
     static public string startingTileType = "sand";
     private GameObject[,] mapTileSprites = new GameObject[mapSize, mapSize];
-    // private GameObject[,] mapTileEdgeSprites = new GameObject[mapSize + 2, mapSize + 2];
+    private GameObject[,] mapTileEdgeSprites = new GameObject[mapSize + 2, mapSize + 2];
     private string[,] mapTileTypes = new string[mapSize, mapSize];
     private Dictionary<string, Dictionary<string, Sprite[]>> groundTypes = new Dictionary<string, Dictionary<string, Sprite[]>>();
     private bool mapDidChange = false;
@@ -23,6 +23,10 @@ public class TileManager : MonoBehaviour
         Sprite sprite = groundTypes[type][position][randSpriteIndex];
         renderer.sprite = sprite;
         return tile;
+    }
+
+    void AddEdgeToEdgeMap(int x, int y, GameObject tile){
+        mapTileEdgeSprites[x + 1, y + 1] = tile;  
     }
 
     // Start is called before the first frame update
@@ -78,38 +82,46 @@ public class TileManager : MonoBehaviour
                     if(x <= 0 || mapTileTypes[x - 1 , y] != mapTileTypes[x, y]){
                         GameObject tile = CreateTile(grid, x - 1, y, startingTileType, "edge");
                         tile.transform.Rotate(new Vector3(0, 0, 90));
+                        AddEdgeToEdgeMap(x - 1, y , tile);
                         emptyW = true;
                     }
                     if(x >= mapSize - 1 || mapTileTypes[x + 1 , y] != mapTileTypes[x, y]){
                         GameObject tile = CreateTile(grid, x + 1, y, startingTileType, "edge");
                         tile.transform.Rotate(new Vector3(0, 0, -90));
+                        AddEdgeToEdgeMap(x + 1, y , tile);
                         emptyE = true;
                     }
                     if(y <= 0 || mapTileTypes[x , y - 1] != mapTileTypes[x, y]){
                         GameObject tile = CreateTile(grid, x, y - 1, startingTileType, "edge");
                         tile.transform.Rotate(new Vector3(0, 0, 180));
+                        AddEdgeToEdgeMap(x, y - 1, tile);
                         emptyS = true;
                     }
                     if(y >= mapSize - 1|| mapTileTypes[x, y + 1] != mapTileTypes[x, y]){
                         GameObject tile = CreateTile(grid, x, y + 1, startingTileType, "edge");
                         // tile.transform.Rotate(new Vector3(0, 0, 0));
+                        AddEdgeToEdgeMap(x, y + 1, tile);
                         emptyN = true;
                     }
                     if(emptyN && emptyE){
                         GameObject tile = CreateTile(grid, x + 1, y + 1, startingTileType, "corner");
                         tile.transform.Rotate(new Vector3(0, 0, -90));
+                        AddEdgeToEdgeMap(x + 1, y + 1 , tile);
                     }
                     if(emptyE && emptyS){
                         GameObject tile = CreateTile(grid, x + 1, y - 1, startingTileType, "corner");
                         tile.transform.Rotate(new Vector3(0, 0, 180));
+                        AddEdgeToEdgeMap(x + 1, y - 1 , tile);
                     }
                     if(emptyS && emptyW){
                         GameObject tile = CreateTile(grid, x - 1, y - 1, startingTileType, "corner");
                         tile.transform.Rotate(new Vector3(0, 0, 90));
+                        AddEdgeToEdgeMap(x - 1, y - 1 , tile);
                     }
                     if(emptyW && emptyN){
-                        GameObject tile = CreateTile(grid, x -1, y + 1, startingTileType, "corner");
+                        GameObject tile = CreateTile(grid, x - 1, y + 1, startingTileType, "corner");
                         // tile.transform.Rotate(new Vector3(0, 0, 0));
+                        AddEdgeToEdgeMap(x - 1, y + 1, tile);
                     }
                 }
             }
