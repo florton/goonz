71a37dc37bd36ee414bcbd336e0c77340e874a3a
RingDingus <RingDingus@gmail.com>
2020-01-21 22:34:27 -0500

    Primitive Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Play game
+    public void PlayGame()
+    {
+        // Go to the next scene in the queue (should be the main)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        // Closes the game
+        Application.Quit();
+    }
+}
