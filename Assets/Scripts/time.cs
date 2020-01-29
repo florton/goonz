a4485fd292f09671dbcb344231fb25de69b10eca
RingDingus <RingDingus@gmail.com>
2020-01-28 21:49:51 -0500

    Revise time; change unity version

--- Assets/Scripts/time.cs
+++ Assets/Scripts/time.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class time : MonoBehaviour 
+public class time : MonoBehaviour
 {
-    private float day = .5f;
+    private float day = 0f;
     public float min;
     public float hours;
     private const float DaytoSecRat = 600f;
@@ -33,32 +33,34 @@ public class time : MonoBehaviour
 
         float daynorm = day % 1f;
 
-        hours = Mathf.Floor((daynorm * 24f) + 1);
+        hours = Mathf.Floor((daynorm * 24f));
+
+        if (hours < 1) {
+            hours = 12;
+        }
 
         hrsclock.text = Mathf.Round(hours).ToString();
 
         min = ((daynorm * 24f) % 1f) * 60f;
 
-        
-
         minclock.text = Mathf.Round(min).ToString("00");
 
-
-
-        if (daynorm <= 0.5)
+        if (daynorm < 0.5)
         {
             sun = daynorm * 2f;
             am.text = ("am");
         }
-        else { 
-            sun = (1f - daynorm) * 2f; 
+        else {
+            sun = (1f - daynorm) * 2f;
             am.text = ("pm");
-            hours = hours - 12;
+            if (hours > 12) {
+                hours = hours - 12;
+            }
         }
 
         hrsclock.text = Mathf.Round(hours).ToString();
 
         GetComponent<Light>().color = new Color ( .5f * sun * 2, .5f * sun * 2, .8f, 1f);
-     } 
-    
+     }
+
 }
